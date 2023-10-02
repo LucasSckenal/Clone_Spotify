d247d21fe42f64365981738ec051164a880cbb38
lucas <lucaspsckenal@gmail.com>
2023-10-02 18:44:19 -0300

    refactor: rename and add link

--- react_music/src/app/Components/Sidebar/Sidebar.tsx
+++ react_music/src/app/Components/Sidebar/Sidebar.tsx
@@ -1,25 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./styles.module.sass"
+import styles from "./styles.module.sass";
 
 const Sidebar = () => {
   return (
     <aside className={styles.bg}>
       <nav>
         <ul>
-          <li>
-            <Link href="/" />
-            Home
-          </li>
-          <li>
-            <Link href="/search" />
-            Search
-          </li>
-          <li>
-            <Image src="/images/library.svg" alt="" width={26} height={26} />
-            <Link href="/library" />
-            Your Library
-          </li>
+          <Link href="/">
+            <li>
+              <Image
+                src="/images/home-fill.svg"
+                alt=""
+                width={26}
+                height={26}
+              />
+              Home
+            </li>
+          </Link>
+          <Link href="/search">
+            <li>
+              <Image src="/images/search.svg" alt="" width={26} height={26} />
+              Search
+            </li>
+          </Link>
+          <Link href="/library">
+            <li>
+              <Image src="/images/library.svg" alt="" width={26} height={26} />
+              Your Library
+            </li>
+          </Link>
         </ul>
       </nav>
     </aside>
