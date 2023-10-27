783b775b73149571c3b34ccddde0b3acefdccd10
Lucas Hack <lucashackcostabeber@gmail.com>
2023-10-27 11:09:33 -0300

    add: functions to login, need to fix

--- react_music/src/app/auth/login/page.tsx
+++ react_music/src/app/auth/login/page.tsx
@@ -1,10 +1,16 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import "./background.css";
 import Image from "next/image";
 import Logo from "../../../../public/images/spotify-logo-0.png";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 import Link from "next/link";
 import { db } from "@/app/api/firebase";
 
@@ -21,10 +27,75 @@ async function fetchDataFromFirestore() {
   }
 }
 
+const createUserFormSchema = z.object({
+  username: z.string().nonempty("Nome de usuario é obrigatório"),
+  email: z
+    .string()
+    .nonempty("O email é obrigatório")
+    .email("formato de email inválido"),
+  birth: z.string().refine(
+    (value) => {
+      const birthDate = new Date(value);
+      const minAgeDate = new Date();
+      minAgeDate.setFullYear(minAgeDate.getFullYear() - 18);
+      return birthDate <= minAgeDate;
+    },
+    {
+      message: "Você deve ter pelo menos 18 anos de idade.",
+    }
+  ),
+  password: z.string().nonempty("Precisa de uma senha"),
+  confirmPassword: z.string().nonempty("A senha precisa ser igual"),
+});
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
+
 function Login() {
   useEffect(() => {
     fetchDataFromFirestore();
   }, []);
+
+  const [showPassword, setShowPassword] = useState<any>("password");
+  const [passwordImg, setPasswordImg] = useState<any>(<AiFillEye />);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
+
+  async function FormSubmit(e: any) {
+    if (e.password == e.confirmPassword) {
+      const name: string = e.username;
+      const email: string = e.email;
+      const birth: string = e.birth;
+      const password: string = e.password;
+
+      const userCollections = collection(db, "users");
+
+      const user = await addDoc(userCollections, {
+        name,
+        email,
+        birth,
+        password,
+      });
+
+      alert("deu certo");
+    } else {
+      alert("não deu certo");
+    }
+  }
+
+  function onClickPassword(e: any): void {
+    e.preventDefault();
+    setShowPassword(showPassword === "password" ? "text" : "password");
+
+    setPasswordImg(
+      showPassword === "password" ? <AiFillEyeInvisible /> : <AiFillEye />
+    );
+  }
 
   return (
     <div className={styles.containerLogin}>
@@ -35,9 +106,18 @@ function Login() {
           <Link href="/auth/register">SIGN UP</Link>
         </div>
 
-        <form action="">
-          <input type="text" placeholder="Username" />
-          <input type="text" placeholder="Password" />
+        <form onSubmit={handleSubmit(FormSubmit)}>
+          <input type="text" placeholder="Username" {...register("username")} />
+          {errors.username && <span>{errors.username.message}</span>}
+          <div className={styles.passwordContainer}>
+            <input
+              type={showPassword}
+              placeholder="Password"
+              {...register("password")}
+            />
+            {errors.password && <span>{errors.password.message}</span>}
+            <button onClick={onClickPassword}>{passwordImg}</button>
+          </div>
 
           <div className={styles.checkbox}>
             <input type="checkbox" name="" id="" />
