286521b514338f2998bd2bdc3ab45565c087659f
Lucas Hack <lucashackcostabeber@gmail.com>
2024-02-17 16:53:00 -0300

    add: more formatation

--- react_music/src/app/Components/Center/Center.tsx
+++ react_music/src/app/Components/Center/Center.tsx
@@ -1,16 +1,32 @@
 import styles from "./styles.module.scss";
-
+import { SlArrowLeft, SlArrowRight, SlArrowDown } from "react-icons/sl";
+import Image from "next/image";
 const Center = () => {
   return (
     <div className={styles.backG}>
       <section className={styles.container}>
         <div className={styles.onTopBtn}>
-          <div>
-            <button>seta esquerda</button>
-            <button>seta direita</button>
+          <div className={styles.arrows}>
+            <button>
+              <SlArrowLeft />
+            </button>
+            <button>
+              <SlArrowRight />
+            </button>
           </div>
           <div>
-            <button className={styles.btnUser}>Foto e nome do usuario</button>
+            <button className={styles.btnUser}>
+              <Image
+                src="/images/fotominha.jpeg"
+                alt=""
+                width={30}
+                height={30}
+              />
+              <p>Nome da pessoa</p>
+              <span>
+                <SlArrowDown />
+              </span>
+            </button>
           </div>
         </div>
       </section>
