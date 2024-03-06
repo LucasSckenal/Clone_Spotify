aa93f02da8a1c8a285006fa3ecb30745a5e17fe3
Lucas Hack <lucashackcostabeber@gmail.com>
2024-03-06 15:43:37 -0300

    add: banner playlist style

--- react_music/src/app/Components/Header/Header.tsx
+++ react_music/src/app/Components/Header/Header.tsx
@@ -4,8 +4,11 @@ import { SlArrowLeft, SlArrowRight, SlArrowDown } from "react-icons/sl";
 import Image from "next/image";
 import { useState } from "react";
 const Header = () => {
-  
-  const [openUser, setOpenUser] = useState();
+  const [openUser, setOpenUser] = useState(false);
+
+  const toggleUser = () => {
+    setOpenUser(!openUser);
+  };
 
   return (
     <div className={styles.backG}>
@@ -19,16 +22,37 @@ const Header = () => {
               <SlArrowRight />
             </button>
           </div>
-
-          <button className={styles.btnUser}>
-            <Image src="/images/fotominha.jpeg" alt="" width={30} height={30} />
-            <p>Nome da pessoa</p>
-            <span>
-              <SlArrowDown />
-            </span>
+          <button
+            onClick={toggleUser}
+            className={`${styles.btnUser} ${openUser ? styles.open : ""}`}
+          >
+            <div>
+              <Image
+                src="/images/fotominha.jpeg"
+                alt=""
+                width={30}
+                height={30}
+              />
+              <p>Nome da pessoa</p>
+              <span>
+                <SlArrowDown />
+              </span>
+            </div>
           </button>
         </div>
       </header>
+
+      <div className={styles.info}>
+        <Image src="/images/fotoPlaylist.jpg" alt="" width={200} height={200} />
+        <div className={styles.infoText}>
+          <p>PLAYLIST</p>
+          <h1>Titulo da Playlist</h1>
+          <div>
+            <Image src="/images/fotominha.jpeg" alt="" width={35} height={35} />
+            <p>Nome da pessoa</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
