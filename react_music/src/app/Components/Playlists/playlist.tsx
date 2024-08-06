b6ba34b27a45ed4bef146a9c0f34839306f36617
lucasSckenal <lucaspsckenal@gmail.com>
2024-08-06 00:17:29 -0300

    add playlists

---
+++ react_music/src/app/Components/Playlists/playlist.tsx
@@ -0,0 +1,19 @@
+const playlist = () => {
+  return (
+    <main>
+      <div className="topDetails">
+        <div>
+          <input type="image" />
+          <img src="" alt="" />
+        </div>
+        <div className="tileAndDetails">
+          <h2>title</h2>
+          <img src="" alt="" />
+          <p>name * 1 saves * 1 songs, 1 hr 20 min</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default playlist
